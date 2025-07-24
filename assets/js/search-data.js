e56bbeee7913b04260730bfa4a43d63d165960c1
mingyfu <93852808+mingyfu@users.noreply.github.com>
2025-07-24 23:04:05 +0000

    Deploying to gh-pages from @ mingyfu/mingyfu.github.io@4ba0af4ace2a76b6bd6a9c9e11c5e562f614fbba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,28 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
+  },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
               section: "Dropdown",
